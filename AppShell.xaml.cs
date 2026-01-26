e9c3fbc9760a6bbf891dbfa60ce21de235b97878
Gerald Versluis <gerald.versluis@microsoft.com>
2026-01-26 13:55:37 +0100

    Initial commit: MAUI app with custom NuGet artifacts

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiXamlCsharpSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiXamlCsharpSample;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
